7f4416975a0c98b9f7d69ac309561d44dde62cb0
Igor Zhilin <pathfinderchemical@gmail.com>
2020-06-22 22:01:01 +0400

    Update create.js

--- bundles/debug/commands/create.js
+++ bundles/debug/commands/create.js
@@ -12,14 +12,27 @@ module.exports = {
       return Broadcast.sayAt(player, 'Нужно указать id предмета.');
     }
 
-    const target = args;
+    const [target, quality ] = args.split(' ');
     const isItem = target.includes(':');
     let targetItem = null;
 
     if (isItem) {
       targetItem = player.room.spawnItem(state, target);
-//      player.addItem(EnhanceItem.enhance(targetItem, 'artifact'));
-      player.addItem(targetItem);
+      if (!quality) {
+        player.addItem(targetItem);
+      } else if ( quality === 'uncommon' ) {
+        player.addItem(EnhanceItem.enhance(targetItem, 'uncommon'));
+      } else if ( quality === 'rare' ) {
+        player.addItem(EnhanceItem.enhance(targetItem, 'rare'));
+      } else if ( quality === 'epic' ) {
+        player.addItem(EnhanceItem.enhance(targetItem, 'epic'));
+      } else if ( quality === 'legendary' ) {
+        player.addItem(EnhanceItem.enhance(targetItem, 'legendary'));
+      } else if ( quality === 'artifact' ) {
+        player.addItem(EnhanceItem.enhance(targetItem, 'artifact'));
+      } else {
+        Broadcast.sayAt(player, 'Неверный формат качества предмета.');
+      }
       player.room.removeItem(targetItem);
       Broadcast.sayAt(player, `Вы создали ${targetItem.name}.`);
     } else {
