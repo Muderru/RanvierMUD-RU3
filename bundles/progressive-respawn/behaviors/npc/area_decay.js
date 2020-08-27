70390740be26c7950be190cc7e9fcc67fc8ee3bd
Igor Zhilin <pathfinderchemical@gmail.com>
2020-08-27 22:49:56 +0400

    небольшое исправление

--- bundles/progressive-respawn/behaviors/npc/area_decay.js
+++ bundles/progressive-respawn/behaviors/npc/area_decay.js
@@ -1,9 +1,14 @@
 const { Logger } = require('ranvier');
 
+/**
+ * Только для мобов, которые создаются с помощью триггера. Для других мобов
+ * будут ошибки.
+ */
 module.exports = {
   listeners: {
     respawnTick: (state) => function () {
       Logger.verbose(`${this.id} has decayed on zone reset.`);
+      this.room.removeNpc(this, true);
       state.MobManager.removeMob(this);
     },
   },
